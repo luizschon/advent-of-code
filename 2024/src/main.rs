19b60ab45be75d7780c9cd9e4603c53f7df2f935
Luiz Schonarth <luizcschonarth@gmail.com>
2025-04-09 17:34:06 -0300

    Change cli

--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -1,4 +1,4 @@
-use clap::{error::ErrorKind, CommandFactory, Parser};
+use clap::Parser;
 use std::fs;
 
 pub mod days;
@@ -6,9 +6,6 @@ pub mod days;
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    #[arg(short = 'a', long = "all")]
-    run_all: bool,
-
     #[arg(short, long)]
     day: Option<u32>,
 }
@@ -17,7 +14,14 @@ fn main() {
     let registry = register_days!(day_01, day_02);
     let cli = Cli::parse();
 
-    if cli.run_all {
+    if let Some(day) = cli.day {
+        if let Some(solution) = registry.get(day) {
+            let input = fs::read_to_string(format!("input/day_{:02}", day)).unwrap();
+            solution.run_parts(&input, day);
+        } else {
+            eprintln!("Day {} not registered", day);
+        }
+    } else {
         let mut first_run = true;
         for (&day, solution) in registry.solutions.iter() {
             if !first_run {
@@ -26,22 +30,6 @@ fn main() {
             let input = fs::read_to_string(format!("input/day_{:02}", day)).unwrap();
             solution.run_parts(&input, day);
             first_run = false;
-        }
-    } else {
-        let Some(day) = cli.day else {
-            Cli::command()
-                .error(
-                    ErrorKind::ArgumentConflict,
-                    "Setting --day is required when not using --all",
-                )
-                .exit()
-        };
-
-        if let Some(solution) = registry.get(day) {
-            let input = fs::read_to_string(format!("input/day_{:02}", day)).unwrap();
-            solution.run_parts(&input, day);
-        } else {
-            Cli::command().error(ErrorKind::InvalidValue, "Day {} not registered");
         }
     }
 }
