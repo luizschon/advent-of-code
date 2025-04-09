ba3f99d59d4b8423fbdfbef9137df3b22e7fa525
Luiz Schonarth <luizcschonarth@gmail.com>
2025-04-09 17:08:10 -0300

    Add day 2 solution

--- 2024/src/days/day_02.rs
+++ 2024/src/days/day_02.rs
@@ -1,9 +1,59 @@
 use std::fmt::Display;
 
-pub fn part_1(_input: &str) -> Box<dyn Display> {
-    Box::new(0)
+fn is_safe(report: &[u64]) -> bool {
+    if report.len() <= 1 {
+        return true;
+    }
+
+    let increasing = report[0] > report[1];
+    report.windows(2).all(|w| {
+        let (a, b) = (w[0], w[1]);
+        a != b && a.abs_diff(b) <= 3 && (a > b) == increasing
+    })
 }
 
-pub fn part_2(_input: &str) -> Box<dyn Display> {
-    Box::new(0)
+fn is_safe_damped(report: &[u64]) -> bool {
+    is_safe(report)
+        || (0..report.len()).any(|i| {
+            is_safe(
+                &report[..i]
+                    .iter()
+                    .chain(report[i + 1..].iter())
+                    .copied()
+                    .collect::<Vec<_>>(),
+            )
+        })
+}
+
+pub fn part_1(input: &str) -> Box<dyn Display> {
+    let reports = parse(input);
+
+    Box::new(
+        reports
+            .iter()
+            .map(|r| is_safe(r))
+            .fold(0, |acc, b| acc + b as u64),
+    )
+}
+
+pub fn part_2(input: &str) -> Box<dyn Display> {
+    let reports = parse(input);
+
+    Box::new(
+        reports
+            .iter()
+            .map(|r| is_safe_damped(r))
+            .fold(0, |acc, b| acc + b as u64),
+    )
+}
+
+fn parse(input: &str) -> Vec<Vec<u64>> {
+    input
+        .lines()
+        .map(|l| {
+            l.split_whitespace()
+                .filter_map(|s| s.parse().ok())
+                .collect()
+        })
+        .collect()
 }
