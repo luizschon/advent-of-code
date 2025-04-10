4f07d005171c262d18a4552fffe776236bdd5216
Luiz Schonarth <luizcschonarth@gmail.com>
2025-04-10 17:38:36 -0300

    Handle missing input files from main

--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -1,7 +1,8 @@
 use clap::Parser;
-use std::fs;
+use std::{fs, path::Path};
 
 use crate::days::DAY_SOLUTIONS;
+use colored::Colorize;
 
 pub mod days;
 
@@ -12,12 +13,26 @@ struct Cli {
     day: Option<u32>,
 }
 
+fn read_day_input(path: &str, day: u32) -> Option<String> {
+    if let Ok(input) = fs::read_to_string(path) {
+        Some(input)
+    } else {
+        eprintln!(
+            "{}",
+            format!("Missing input file for day {day}, consider adding it to the input dir.").red()
+        );
+        None
+    }
+}
+
 fn main() {
     let cli = Cli::parse();
 
     if let Some(day) = cli.day {
         if let Some(solution) = DAY_SOLUTIONS.get(&day) {
-            let input = fs::read_to_string(format!("input/day_{:02}", day)).unwrap();
+            let Some(input) = read_day_input(&format!("input/day_{:02}", day), day) else {
+                return;
+            };
             solution.run_parts(&input, day);
         } else {
             eprintln!("Day {} not registered", day);
@@ -29,7 +44,9 @@ fn main() {
             if !first_run {
                 print!("\n");
             }
-            let input = fs::read_to_string(format!("input/day_{:02}", day)).unwrap();
+            let Some(input) = read_day_input(&format!("input/day_{:02}", day), day) else {
+                continue;
+            };
             solution.run_parts(&input, day);
             first_run = false;
         }
